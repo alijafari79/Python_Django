62a7ffe72ead4b04e03b9467b5f172019b287365
alijafari79 <alijafari13791397@gmail.com>
2022-07-14 20:47:14 +0430

    get request info

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse as hr
 # Create your views here.
 
-def home_view(*args, **kwargs) :
+def home_view(request, *args, **kwargs) :
+	#print(request)
+	print(request.user) # will be "AnanimousUser" if user requesting is not logged in !
 	return hr("<h1>Hello World</h1>")
 
+def contact_view(request,*args, **kwargs) :
+	return hr("<h1>Contact page</h1>")
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, contact_view
+
 #from products.views import product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name="home")
+    path('', home_view, name="home"),
+    path('contact/', contact_view, name="contact")
 ]
