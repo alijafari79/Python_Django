79b033fe518e6ead9e5d258835929739b6b36f4d
Ali Jafari <alijafari13791397@gmail.com>
2022-07-22 16:30:02 +0430

    render .html file view

--- pages/views.py
+++ pages/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse as hr
 # Create your views here.
 
 def home_view(request, *args, **kwargs) :
+	print(args)
 	print(">> User: ", request.user)
 	#print(request.user) # will be "AnanimousUser" if user requesting is not logged in !
-	return hr("<h1>Hello World</h1>")
+	
+	# return hr("<h1>Hello World</h1>")
+	return render(request, "home.html", {})
 
 def contact_view(request,*args, **kwargs) :
 	return hr("<h1>Contact page</h1>")
